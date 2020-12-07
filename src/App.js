75d3320cbad7731bc2e69279774fed14a0aa8e70
Benoit Hubert <benoithubert@gmail.com>
2020-12-07 08:25:46 +0100

    Fetch movies from API

--- src/App.js
+++ src/App.js
@@ -1,30 +1,38 @@
+import { Component } from 'react';
+import axios from 'axios';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
 import './App.css';
 
-const movies = [
-  {
-    id: 1,
-    title: 'The Invisible Man',
-    picture:
-      'https://see-mag.fr/wp-content/uploads/2020/06/THE_INVISIBLE_MAN_POSTER_SEE.jpg',
-    release: '2020-02-28',
-  },
-  {
-    id: 2,
-    title: 'Inception',
-    picture:
-      'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_.jpg',
-    release: '2010-07-08',
-  },
-];
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: [],
+    };
+  }
 
-function App() {
-  return (
-    <div className="App container">
-      <Header />
-      <MovieList movies={movies} />
-    </div>
-  );
+  componentDidMount() {
+    this.fetchMovies();
+  }
+
+  fetchMovies() {
+    const apiUrl =
+      'https://my-json-server.typicode.com/bhubr/micro-movie-api/movies';
+    axios
+      .get(apiUrl)
+      .then((res) => res.data)
+      .then((movies) => this.setState({ movies }));
+  }
+
+  render() {
+    const { movies } = this.state;
+    return (
+      <div className="App container">
+        <Header />
+        <MovieList movies={movies} />
+      </div>
+    );
+  }
 }
 export default App;
