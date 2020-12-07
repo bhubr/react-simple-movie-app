964a344ab6d1896d340633fefbb5ded39e42612e
Benoit Hubert <benoithubert@gmail.com>
2020-12-07 08:25:18 +0100

    MovieList component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import './App.css';
 import Header from './components/Header';
-import Movie from './components/Movie';
+import MovieList from './components/MovieList';
+import './App.css';
 
-const movie = {
-  id: 1,
-  title: 'The Invisible Man',
-  picture:
-    'https://see-mag.fr/wp-content/uploads/2020/06/THE_INVISIBLE_MAN_POSTER_SEE.jpg',
-  release: '2020-02-28',
-};
+const movies = [
+  {
+    id: 1,
+    title: 'The Invisible Man',
+    picture:
+      'https://see-mag.fr/wp-content/uploads/2020/06/THE_INVISIBLE_MAN_POSTER_SEE.jpg',
+    release: '2020-02-28',
+  },
+  {
+    id: 2,
+    title: 'Inception',
+    picture:
+      'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_.jpg',
+    release: '2010-07-08',
+  },
+];
 
 function App() {
   return (
     <div className="App container">
       <Header />
-      <Movie movie={movie} />
+      <MovieList movies={movies} />
     </div>
   );
 }
-
 export default App;
